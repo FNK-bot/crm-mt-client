8668f00e13c851a178fc917586c2923ad56045ce
Farseen N k <farseennk2@gmail.com>
2025-03-14 14:35:27 +0530

    six

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -18,38 +18,3 @@ axiosInstance.interceptors.request.use((config) => {
 });
 
 export default axiosInstance;
-
-// import axios from "axios";
-
-// const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-
-// const axiosInstance = axios.create({
-//   baseURL: BASE_URL,
-//   headers: { "Content-Type": "application/json" },
-// });
-
-// // ✅ Attach JWT token automatically
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-// // ✅ Handle 401 Unauthorized errors (Token Expired)
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response && error.response.status === 401) {
-//       localStorage.removeItem("token");
-//       window.location.href = "/login";
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default axiosInstance;
