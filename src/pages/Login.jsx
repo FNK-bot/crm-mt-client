67ab6f3c994919c968b19681dbd14b74dc8f85dd
Farseen N k <farseennk2@gmail.com>
2025-03-14 14:29:06 +0530

    five

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,24 +2,27 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { loginSuccess } from '../redux/slices/authSlice';
 import { useNavigate ,Link} from 'react-router-dom';
-import { Container, TextField, Button, Typography } from '@mui/material';
+import { Container, TextField, Button, Typography, CircularProgress } from '@mui/material';
 import axiosInstance from '../api/axiosInstance';
 import {ToastContainer,toast} from 'react-toastify';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
+      setLoading(true)
       const res = await axiosInstance.post('/login', { email, password });
       dispatch(loginSuccess(res.data));
       navigate('/dashboard');
+      setLoading(false)
     } catch (error) {
-      console.log(error)
+      setLoading(false)
       toast.error("Invalid credentials");
     }
   };
@@ -30,7 +33,7 @@ const Login = () => {
       <form onSubmit={handleLogin}>
         <TextField label="Email" fullWidth margin="normal" value={email} onChange={(e) => setEmail(e.target.value)} required />
         <TextField label="Password" type="password" fullWidth margin="normal" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        <Button type="submit" variant="contained" color="primary" fullWidth>Login</Button>
+        <Button type="submit" variant="contained" color="primary" fullWidth>{loading ? <CircularProgress size={24} /> : "Login"}</Button>
       </form>
       <Typography variant="body2" sx={{ mt: 2, textAlign: "center" }}>
         Don't have an account? <Link to="/register">Register</Link>
